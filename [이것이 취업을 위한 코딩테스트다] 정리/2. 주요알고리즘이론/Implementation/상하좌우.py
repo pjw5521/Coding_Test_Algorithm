8c7ef139b32c46959ded0d28864112e4a73c9038
pjw5521 <pjw5521@naver.com>
2021-09-27 17:54:33 +0900

    책 구현 예제 풀이 중

---
+++ [이것이 취업을 위한 코딩테스트다] 정리/2. 주요알고리즘이론/Implementation/상하좌우.py
@@ -0,0 +1,23 @@
+n = int(input())
+x, y = 1,1 
+plans = input().split()
+
+for i in plans:
+  if( i == "R"):
+    if( y == n ):
+      continue
+    y += 1
+  elif( i == "U"):
+    if( x == 1 ):
+      continue
+    x -= 1
+  elif( i == "L"):
+    if( y == 1 ):
+      continue
+    y -= 1
+  elif( i == "D"):
+    if( x == n  ):
+      continue
+    x += 1
+
+print(x,y)
